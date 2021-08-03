44d9f7b91c1beb58d25b0d4f6b70ddd1f8690db0
Piyush <pallavi@wbcomdesigns.com>
2021-08-03 13:01:36 +0530

    shift default option code

--- includes/class-email-customizer-for-woocommerce-activator.php
+++ includes/class-email-customizer-for-woocommerce-activator.php
@@ -30,6 +30,24 @@ class Email_Customizer_For_Woocommerce_Activator {
 	 */
 	public static function activate() {
 
+		// Save current email settings.
+		$header_image     = get_option( 'woocommerce_email_header_image', 'http://' );
+		$footer_text      = get_option( 'woocommerce_email_footer_text', wp_specialchars_decode( get_bloginfo( 'name' ), ENT_QUOTES ) . ' - Powered by WooCommerce' );
+		$base_color       = get_option( 'woocommerce_email_base_color', '#557da1' );
+		$background_color = get_option( 'woocommerce_email_background_color', '#f5f5f5' );
+		$body_bg_color    = get_option( 'woocommerce_email_body_background_color', '#fdfdfd' );
+		$text_color       = get_option( 'woocommerce_email_text_color', '#505050' );
+
+		$settings = array(
+			'woocommerce_email_header_image'          => $header_image,
+			'woocommerce_email_footer_text'           => $footer_text,
+			'woocommerce_email_base_color'            => $base_color,
+			'woocommerce_email_background_color'      => $background_color,
+			'woocommerce_email_body_background_color' => $body_bg_color,
+			'woocommerce_email_text_color'            => $text_color,
+		);
+
+		update_option( 'wc_email_customizer_old_settings', $settings );
 	}
 
 }
--- woocommerce-email-customizer.php
+++ woocommerce-email-customizer.php
@@ -61,33 +61,8 @@ if ( ! defined( 'EMAIL_CUSTOMIZER_FOR_WOOCOMMERCE_PLUGIN_FILE' ) ) {
 function activate_email_customizer_for_woocommerce() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-email-customizer-for-woocommerce-activator.php';
 	Email_Customizer_For_Woocommerce_Activator::activate();
-	do_action( 'wb_email_customizer_update_option' );
 }
-/**
- * Activation tasks.
- */
-function wb_email_customizer_update_option() {
-	// Save current email settings.
-	$header_image     = get_option( 'woocommerce_email_header_image', 'http://' );
-	$footer_text      = get_option( 'woocommerce_email_footer_text', wp_specialchars_decode( get_bloginfo( 'name' ), ENT_QUOTES ) . ' - Powered by WooCommerce' );
-	$base_color       = get_option( 'woocommerce_email_base_color', '#557da1' );
-	$background_color = get_option( 'woocommerce_email_background_color', '#f5f5f5' );
-	$body_bg_color    = get_option( 'woocommerce_email_body_background_color', '#fdfdfd' );
-	$text_color       = get_option( 'woocommerce_email_text_color', '#505050' );
 
-	$settings = array(
-		'woocommerce_email_header_image'          => $header_image,
-		'woocommerce_email_footer_text'           => $footer_text,
-		'woocommerce_email_base_color'            => $base_color,
-		'woocommerce_email_background_color'      => $background_color,
-		'woocommerce_email_body_background_color' => $body_bg_color,
-		'woocommerce_email_text_color'            => $text_color,
-	);
-
-	update_option( 'wc_email_customizer_old_settings', $settings );
-
-	return true;
-}
 /**
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-email-customizer-for-woocommerce-deactivator.php
