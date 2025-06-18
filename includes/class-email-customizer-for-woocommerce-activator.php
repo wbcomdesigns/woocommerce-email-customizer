4f592fc297b268e72e7803b1a64681edc57788a8
Divang <pallavi@wbcomdesigns.com>
2025-06-18 14:58:46 +0530

    default email layout UI issue on mail.
    https://3.basecamp.com/5798509/buckets/42374752/card_tables/cards/8767864181

--- includes/class-email-customizer-for-woocommerce-activator.php
+++ includes/class-email-customizer-for-woocommerce-activator.php
@@ -30,6 +30,23 @@ class Email_Customizer_For_Woocommerce_Activator {
 	 */
 	public static function activate() {
 
+		if(!get_option( 'woocommerce_email_template' )){
+			update_option('woocommerce_email_template','default');
+			update_option( 'woocommerce_email_header_text_color', '#ffffff',true ) ;
+			update_option( 'woocommerce_email_body_background_color', '#fdfdfd',true ) ;
+			update_option( 'woocommerce_email_header_background_color', '#557da1',true ) ;
+			update_option( 'woocommerce_email_footer_address_background_color', '#ffffff',true ) ;
+			update_option( 'woocommerce_email_footer_address_border', '1',true ) ;
+			update_option( 'woocommerce_email_rounded_corners', '6',true ) ;
+			update_option( 'woocommerce_email_border_container_top', '1',true ) ;
+			update_option( 'woocommerce_email_border_container_bottom','1',true ) ;
+			update_option( 'woocommerce_email_border_container_left', '1',true ) ;
+			update_option( 'woocommerce_email_border_container_right', '1',true ) ;
+			update_option( 'woocommerce_email_body_border_color', '#505050',true ) ;
+			update_option( 'woocommerce_email_footer_text_color', '#ffffff',true ) ;
+			update_option( 'woocommerce_email_footer_background_color', '#202020',true ) ;	
+		}
+
 		// Save current email settings.
 		$header_image     = get_option( 'woocommerce_email_header_image', 'http://' );
 		$footer_text      = get_option( 'woocommerce_email_footer_text', wp_specialchars_decode( get_bloginfo( 'name' ), ENT_QUOTES ) . ' - Powered by WooCommerce' );
@@ -51,9 +68,13 @@ class Email_Customizer_For_Woocommerce_Activator {
 
 
 		$default_array = get_option('wb_email_customizer_old');
+		if( ! empty( $default_array ) ){
 			foreach ($default_array as $key => $value) {
 				update_option( $key, $value ,true ) ;
 			}
+			delete_option( 'wb_email_customizer_old' );
+		}
+			
 	}
 
 }
